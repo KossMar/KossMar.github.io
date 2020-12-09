8177c720e979df596db037f2d3e304f2ac6770bf
Mar Koss <mar.koss@icloud.com>
2020-12-09 14:53:38 -0800

    added collapsible project info

--- app.js
+++ app.js
@@ -103,3 +103,26 @@ skillsArr.forEach((skillCategory) => {
 });
 
 
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+    coll[i].addEventListener("click", function () { 
+        this.classList.toggle("active");
+
+        var content = this.nextElementSibling;
+        var container = this.parentNode.parentNode.parentNode;
+
+
+        if (this.classList.contains("active")) {
+            content.style.maxHeight =  content.scrollHeight + "px";
+            this.innerHTML = "Less Info"
+            container.style.backgroundColor = "#f7fbfc";
+        } else {
+            content.style.maxHeight = 0;
+            this.innerHTML = "More Info"
+            container.style.backgroundColor = "white";
+        }
+    });
+}
+
