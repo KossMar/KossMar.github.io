6672faa7f86d2d5c078f997902512f09f84b9f7a
Mar Koss <mar.koss@icloud.com>
2021-07-22 14:37:37 -0700

    fixed errors, added gap message, turned logo to link

--- app.js
+++ app.js
@@ -1,108 +1,3 @@
-var skillsArr = [
-    {
-        title: "Languages",
-        skills: [
-            "Swift",
-            "Kotlin",
-            "HTML",
-            "Javascript",
-            "Objective-C",
-            "Java",
-            "CSS"
-        ]
-    },
-    {
-        title: "Frameworks",
-        skills: [
-            "Core Data",
-            "MapKit",
-            "WebKit",
-            "Core Location",
-            "Firebase",
-            "GoogleMaps",
-            "Keychain",
-        ]
-    },
-    {
-        title: "Platforms",
-        skills: [
-            "XCode",
-            "AndroidStudio",
-            "Visual Studio",
-            "Git/Github",
-            "Jira",
-            "Target Process",
-            "Confluence",
-            "AWS",
-            "MongoDB"
-        ]
-    },
-    {
-        title: "Other",
-        skills: [
-            "Photoshop",
-            "Affinity",
-            "Illustrator",
-            "Sketch",
-            "Ableton Live",
-        ]
-    },
-]
-
-var skillsRowDiv = document.getElementById("skillsRowDiv");
-
-skillsArr.forEach((skillCategory) => {
-
-    // Create Skills Col
-    let skillsColDiv = document.createElement("div");
-    skillsColDiv.classList.add("container", "skill-section", "col");
-
-    // Create Skills Col Title
-    let skillsColTitle = document.createElement("h3");
-    skillsColTitle.innerHTML = skillCategory.title;
-
-    // Add Skills Col Title to Skills Col
-    skillsColDiv.appendChild(skillsColTitle);
-
-    // Create Skills Col Body Container
-    let skillsColBodyContainer = document.createElement("div");
-    skillsColBodyContainer.classList.add("container");
-
-    // Create Skills Col Body
-    let skillsColBody = document.createElement("div");
-    skillsColBody.classList.add("row", "row-cols-1", "skills-col-body");
-
-    // Add Skills Col Body to Skills Col Body Container
-    skillsColBodyContainer.appendChild(skillsColBody);
-
-    // Create All Skill Cards
-    skillCategory.skills.forEach((skill) => {
-
-        // Create Individual Skill Card
-        var skillCardDiv = document.createElement("div");
-        skillCardDiv.classList.add("skill-card", "col-lg-4", "col-md-4", "col-sm-6");
-
-        // Create Skill Title
-        var skillTitleDiv = document.createElement("div");
-        skillTitleDiv.classList.add("skill-title");
-        skillTitleDiv.innerHTML = skill;
-
-        // Add Skill Title to Skill Card
-        skillCardDiv.appendChild(skillTitleDiv);
-
-        // Add Skill Card to Skills Col Body
-        skillsColBody.appendChild(skillCardDiv);
-    });
-
-    // Add Skills Col Body Container to Skills Col
-    skillsColDiv.appendChild(skillsColBodyContainer)
-
-    // Add Skills Col to Skills Row
-    skillsRowDiv.appendChild(skillsColDiv);
-
-});
-
-
 var coll = document.getElementsByClassName("collapsible");
 var i;
 
@@ -154,7 +49,7 @@ window.onscroll = () => {
     if (currentScroll < navStart) {
         navOpacity = 0
     } else if (currentScroll >= navStart && currentScroll <= end) {
-        navOpacity = (currentScroll - navStart) / (end - navStart) ;
+        navOpacity = (currentScroll - navStart) / (end - navStart);
         console.log(navOpacity)
     } else if (currentScroll > end) {
         navOpacity = 1
@@ -164,7 +59,7 @@ window.onscroll = () => {
     if (currentScroll < logoStart) {
         logoOpacity = 0
     } else if (currentScroll >= logoStart && currentScroll <= end) {
-        logoOpacity = (currentScroll - logoStart) / (end - logoStart) ;
+        logoOpacity = (currentScroll - logoStart) / (end - logoStart);
         console.log(logoOpacity)
     } else if (currentScroll > end) {
         logoOpacity = 1
@@ -176,4 +71,109 @@ window.onscroll = () => {
     blackLogo.style.opacity = logoOpacity
     //   document.querySelector('.nav-bg'.style.opacity = opacity)
 }
+
+var skillsRowDiv = document.getElementById("skillsRowDiv");
+
+
+var skillsArr = [
+    {
+        title: "Languages",
+        skills: [
+            "Swift",
+            "Kotlin",
+            "HTML",
+            "Javascript",
+            "Objective-C",
+            "Java",
+            "CSS"
+        ]
+    },
+    {
+        title: "Frameworks",
+        skills: [
+            "Core Data",
+            "MapKit",
+            "WebKit",
+            "Core Location",
+            "Firebase",
+            "GoogleMaps",
+            "Keychain",
+        ]
+    },
+    {
+        title: "Platforms",
+        skills: [
+            "XCode",
+            "AndroidStudio",
+            "Visual Studio",
+            "Git/Github",
+            "Jira",
+            "Target Process",
+            "Confluence",
+            "AWS",
+            "MongoDB"
+        ]
+    },
+    {
+        title: "Other",
+        skills: [
+            "Photoshop",
+            "Affinity",
+            "Illustrator",
+            "Sketch",
+            "Ableton Live",
+        ]
+    },
+]
+
+skillsArr.forEach((skillCategory) => {
+
+    // Create Skills Col
+    let skillsColDiv = document.createElement("div");
+    skillsColDiv.classList.add("container", "skill-section", "col");
+
+    // Create Skills Col Title
+    let skillsColTitle = document.createElement("h3");
+    skillsColTitle.innerHTML = skillCategory.title;
+
+    // Add Skills Col Title to Skills Col
+    skillsColDiv.appendChild(skillsColTitle);
+
+    // Create Skills Col Body Container
+    let skillsColBodyContainer = document.createElement("div");
+    skillsColBodyContainer.classList.add("container");
+
+    // Create Skills Col Body
+    let skillsColBody = document.createElement("div");
+    skillsColBody.classList.add("row", "row-cols-1", "skills-col-body");
+
+    // Add Skills Col Body to Skills Col Body Container
+    skillsColBodyContainer.appendChild(skillsColBody);
+
+    // Create All Skill Cards
+    skillCategory.skills.forEach((skill) => {
+
+        // Create Individual Skill Card
+        var skillCardDiv = document.createElement("div");
+        skillCardDiv.classList.add("skill-card", "col-lg-4", "col-md-4", "col-sm-6");
+
+        // Create Skill Title
+        var skillTitleDiv = document.createElement("div");
+        skillTitleDiv.classList.add("skill-title");
+        skillTitleDiv.innerHTML = skill;
+
+        // Add Skill Title to Skill Card
+        skillCardDiv.appendChild(skillTitleDiv);
+
+        // Add Skill Card to Skills Col Body
+        skillsColBody.appendChild(skillCardDiv);
+    });
+
+    // Add Skills Col Body Container to Skills Col
+    skillsColDiv.appendChild(skillsColBodyContainer)
+
+    // Add Skills Col to Skills Row
+    skillsRowDiv.appendChild(skillsColDiv);
+
+});
 
